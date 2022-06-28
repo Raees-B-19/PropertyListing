f0eb5237a0716adc614eefed9eb7148096c275cb
GarlicBuns19 <planterx26@gmail.com>
2022-06-28 14:13:55 +0200

    modal

--- script/propertyListing.js
+++ script/propertyListing.js
@@ -57,7 +57,7 @@ let properties = JSON.parse(localStorage.getItem('property')) ?
 
 
 function getProperties(){
-    properties.forEach((e) => {
+    properties.forEach((e,index) => {
         document.getElementById('properties-row').innerHTML +=
             `<div class="col-md-3 p-3">
             <div class="card" style="width: 18rem;">
@@ -66,12 +66,33 @@ function getProperties(){
               <h4 class="card-title">${e.type} in ${e.location}</h4>
               <h4 class="card-title">${e.bed} ${e.toilet}</h4>
               <h5>${e.price}</h5>
-            </div>
-          </div>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${index}" onclick="modalProperties()">
+                View Property
+               </button>
+            <div class="modal fade" id="exampleModal${index}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                    </div>
+                </div>
             </div>`
     })
 }
 getProperties();
+
 
 // For Search Button
 // function filterByOption(event) {
