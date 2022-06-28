75ea6b557e75d69b99a0bc04d87c2d52a27c8951
GarlicBuns19 <planterx26@gmail.com>
2022-06-28 11:15:47 +0200

    array

--- script/propertyListing.js
+++ script/propertyListing.js
@@ -1,30 +1,40 @@
 let properties = JSON.parse(localStorage.getItem('property')) ?
-JSON.parse(localStorage.getItem('property'))  :  [
-    {
-        id:1,
-        type: 'Apartment',
-        location: 'New York City',
-        url:"https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
-        price: 10000
-    },
-    {
-        id:2,
-        type: 'Apartment',
-        location: 'New York City',
-        url:"https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
-        price: 10000
-    },
-    {
-        id:3,
-        type: 'Apartment',
-        location: 'New York City',
-        url:"https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
-        price: 10000
-    }
-];
-
-properties.forEach((e) => {
-    
-})
+    JSON.parse(localStorage.getItem('property')) : [{
+            id: 1,
+            type: 'Apartment',
+            location: 'New York City',
+            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            price: 10000
+        },
+        {
+            id: 2,
+            type: 'Apartment',
+            location: 'New York City',
+            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            price: 10000
+        },
+        {
+            id: 3,
+            type: 'Apartment',
+            location: 'New York City',
+            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            price: 10000
+        }
+    ];
 
 
+function getProperties(){
+    properties.forEach((e) => {
+        document.getElementById('properties-row').innerHTML +=
+            `<div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+            <img src="${e.url}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h4 class="card-title">${e.type} in ${e.location}</h4>
+              <h5>${e.price}</h5>
+            </div>
+          </div>
+            </div>`
+    })
+}
+getProperties();
