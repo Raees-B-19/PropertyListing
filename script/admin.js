ecdfe92302b329d33b743e80cda6caaa0b55a39c
GarlicBuns19 <planterx26@gmail.com>
2022-06-29 16:21:45 +0200

    filters needed

--- script/admin.js
+++ script/admin.js
@@ -4,7 +4,7 @@ let properties = JSON.parse(localStorage.getItem('property'))
 function tableData(){
     let tb = document.getElementById('table-body');
     tb.innerHTML = '';
-    properties.forEach((td) => {
+    properties.forEach((td,index) => {
         tb.innerHTML += `
             <tr>
                 <td>${td.id}</td>
@@ -14,8 +14,30 @@ function tableData(){
                 <td>${td.bed}</td>
                 <td>${td.toilet}</td>
                 <td>${td.price}</td>
-            </tr>
+                <td><i class="bi bi-pencil-square" onclick="edit(${td.id})"></i></td>
+                <td><i class="bi bi-trash" onclick="remove(${index})"></i></td>
+                </tr>
         `
     })
 }
 tableData();
+
+function edit(id){
+    const newProp = prompt('What would u change')
+    const task = properties.find((property) => property.id === id);
+    task.type = newProp
+    localStorage.setItem('property',JSON.stringify(properties));
+    tableData();
+};
+
+function remove(id) {
+    if (id > -1) {
+      properties.splice(id, 1);
+      // Apply the change
+      localStorage.setItem('property', JSON.stringify(properties));
+    }
+    for(i=0;i<properties.length;i++){
+        properties[i].id = i+1;
+    }
+    tableData();
+};
