1e18afe1cf7b7dcc1d528f9d87c91e0e646bd0fc
GarlicBuns19 <planterx26@gmail.com>
2022-06-29 13:49:39 +0200

    admin almost done

--- script/admin.js
+++ script/admin.js
@@ -1,5 +1,21 @@
-// let test1 = localStorage.getItem('property')
-// console.log(test1)
+let properties = JSON.parse(localStorage.getItem('property'))
+// console.log(JSON.parse(localStorage.getItem('property')))
 
-// let f = localStorage.clear()
-// console.log(f)
+function tableData(){
+    let tb = document.getElementById('table-body');
+    tb.innerHTML = '';
+    properties.forEach((td) => {
+        tb.innerHTML += `
+            <tr>
+                <td>${td.id}</td>
+                <td><img src="${td.url}" alt="" width="100px" height="80px"></td>
+                <td>${td.type}</td>
+                <td>${td.location}</td>
+                <td>${td.bed}</td>
+                <td>${td.toilet}</td>
+                <td>${td.price}</td>
+            </tr>
+        `
+    })
+}
+tableData();
