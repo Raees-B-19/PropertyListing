7f7490abfc6dcea693bdc6c7c9d062ef8e5cc02a
GarlicBuns19 <planterx26@gmail.com>
2022-07-01 15:31:46 +0200

    help

--- script/admin.js
+++ script/admin.js
@@ -11,45 +11,46 @@ function tableData() {
                 <td scope="row">${td.type}</td>
                 <td scope="row">${td.location}</td>
                 <td scope="row">${td.bed}</td>
-                <td scope="row">${td.bathroom}</td>
+                <td scope="row">${td.bathrooms}</td>
                 <td scope="row">${td.price}</td>
                 <!-- Button trigger modal -->
                 <td scope="row">
                 <a class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal${index}">
-                <i class="bi bi-pencil-square"></i></td>
+                <i class="bi bi-pencil-square"></i>
                 </a>
+                </td>
                 <td scope="row"><i class="bi bi-trash" onclick="remove(${index})"></i></td>
-                </tr>
+            </tr>
 
 
 
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal${index}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Edit the Property</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                    <h6>Type of Property</h6>
-                    <input value="${td.type}" id="pType${index}">
-                    <h6>Type of Location</h6>
-                    <input value="${td.location}" id="pLocation${index}">
-                    <h6>Amount of Beds</h6>
-                    <input value="${td.bed}" id="pBed${index}">
-                    <h6>Amount of Toilets</h6>
-                    <input value="${td.bathroom}" id="pToilet${index}">
-                    <h6>Property Price</h6>
-                    <input value="${td.price}" id="pPrice${index}">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" data-bs-dismiss="modal" class="btn btn-primary" onclick="edit(${index})">Save changes</button>
-                    </div>
-                    </div>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal${index}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit the Property</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <h6>Type of Property</h6>
+                <input value="${td.type}" id="pType${index}">
+                <h6>Type of Location</h6>
+                <input value="${td.location}" id="pLocation${index}">
+                <h6>Amount of Beds</h6>
+                <input value="${td.bed}" id="pBed${index}">
+                <h6>Amount of Bathrooms</h6>
+                <input value="${td.bathrooms}" id="pToilet${index}">
+                <h6>Property Price</h6>
+                <input value="${td.price}" id="pPrice${index}">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" data-bs-dismiss="modal" class="btn btn-primary" onclick="edit(${index})">Save changes</button>
                 </div>
                 </div>
+            </div>
+            </div>
 
         `
     })
